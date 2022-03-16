9c5a80e4e082a402d530653a316db07549bf8ab3
Ketan Chawla <500075382@stu.upes.ac.in>
2022-03-17 00:33:25 +0530

    CRUD APPLICATION

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const ejs = require('ejs');
 const port = 5500 
 
 
-const mysql = require('mysql');
+var mysql = require('mysql');
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -66,11 +66,55 @@ app.get('/edit',(req,res)=>{
 app.post('/edit',(req,res)=>{
   let empid=req.body.empid
   let empname=req.body.name;
+  let jobcode=req.body.job_code;
+  let rm=req.body.rm;
+  let ready=req.body.ready;
+  let java=req.body.java;
+  let cpp=req.body.cpp;
 
-  con.query(`UPDATE adobe.panel_members SET emp_name="${empname}" WHERE emp_id=${empid};`, (err,result,field) => 
+  con.query(`UPDATE adobe.panel_members SET emp_name="${empname}", job_code="${jobcode}", rm="${rm}", ready="${ready}", java="${java}", cpp="${cpp}" WHERE emp_id=${empid};`, (err,result,field) => 
     {
     if(err) throw err;
 
+    res.redirect('/excel')
+    });
+
+});
+
+
+
+
+app.get('/delete',(req,res)=>{
+  let empid=req.query.emp_id;
+  con.query(`Delete FROM adobe.panel_members WHERE emp_id=${empid};`, (err,result,field) => 
+    {
+    if(err) throw err;
+
+    res.redirect('/excel')
+    });
+
+});
+
+app.get('/add',(req,res)=>{
+  res.sendFile(__dirname + '/static/add.html');
+});
+
+
+app.post('/add',(req,res)=>{
+  let empid=req.body.empid
+  let empname=req.body.name;
+  let jobcode=req.body.job_code;
+  let rm=req.body.rm;
+  let ready=req.body.ready;
+  let java=req.body.java;
+  let cpp=req.body.cpp;
+
+  console.log(empid);
+
+  con.query(`INSERT INTO adobe.panel_members(emp_id,emp_name,job_code,rm,ready,java,cpp) VALUES (${empid},'${empname}','${jobcode}','${rm}','${ready}','${java}','${cpp}');`, (err,result,field) => 
+
+    {
+    if(err) throw err;  
     res.redirect('/excel')
     });
 
